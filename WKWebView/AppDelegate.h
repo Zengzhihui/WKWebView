90c576659911ee0d0ffa4b8aa894701995bda066
zeng zhihui <zengzhihui@score.cool>
2016-11-02 19:21:08 +0800

    Initial Commit

---
+++ WKWebView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WKWebView
+//
+//  Created by zzh on 16/11/2.
+//  Copyright © 2016年 Gavin Zeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WKWebView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WKWebView
+//
+//  Created by zzh on 16/11/2.
+//  Copyright © 2016年 Gavin Zeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
